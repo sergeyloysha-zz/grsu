6a7e1c33fa2e9db2fb9601076591fbd078b7bb93
Sergey Loysha <sergeyloysha@gmail.com>
2015-09-08 16:12:37 +0300

    update api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ angular.module('teachers', [])
 .controller('mainController', ['$scope', '$http', function($scope, $http) {
   
   // our application code will go here
-	$http.get('http://api.grsu.by/1.x/app1/getTeachers?extended=true').
+	$http.get('teachers.json').
 		then(function(response) {
 			$scope.teachers = response.data.items
 			console.log($scope.teachers)
